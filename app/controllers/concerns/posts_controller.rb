2c4e9685b57521ebe7bc6ff10bf6b1b165d6ed94
Alexey Pakalo <alexey.pakalo@gmail.com>
2017-02-17 23:01:08 +0300

    Innitial commit of the project

---
+++ app/controllers/concerns/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+   before_action :set_post ,only: [:show, :edit, :update, :destroy]
+     def index
+	    @posts = Post.all
+     end
+     def show
+     end	
+     def new
+     	@post = Post.new
+     end
+     def create
+     	@post = Post.new(post_params)
+     	if @post.save
+     		redirect_to @post, success: "Сатья успешно создана"
+     	else
+     		render :new, danger: "Сатья не создана"
+     	end		
+     end
+
+     def edit   	
+     end
+     def update
+     	if @post.update_attributes(post_params)
+     		redirect_to @post, success: 'Статья успешно обновлена'
+    	     else
+     		render :edit , danger: " Статья не обновлена "
+     	end
+
+     end
+     def destroy
+     	@post.destroy
+     	redirect_to posts_path, success: "Сатья успешно удалена"
+     end
+     private
+
+def set_post
+     	@post = Post.find(params[:id])	
+end
+     def post_params
+     	params.require(:post).permit( :title, :summary, :body, :image )
+     end
+end
